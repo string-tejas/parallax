c34c9da0dd4534f240d40d42770c2ba023cd0093
String Tejas <jtjtejas05@gmail.com>
2022-08-02 17:25:51 +0530

    Removed top detect bug

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -5,107 +5,109 @@ import { useKey } from "../custom-hooks";
 export const context = createContext();
 
 export const SlideContainer = ({ className, notify, children, ...others }) => {
-   const ref = useRef();
-   const [offsetY, setOffsetY] = useState(0);
-   const handleScroll = () => setOffsetY(ref.current.scrollTop);
+  const ref = useRef();
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(ref.current.scrollTop);
 
-   useEffect(() => {
-      ref.current.addEventListener("scroll", handleScroll);
-      const refPreserve = ref.current;
-      return () => refPreserve.removeEventListener("scroll", handleScroll);
-   }, []);
+  useEffect(() => {
+    ref.current.addEventListener("scroll", handleScroll);
+    const refPreserve = ref.current;
+    return () => refPreserve.removeEventListener("scroll", handleScroll);
+  }, []);
 
-   const scrollToNextSlide = () => {
-      const top = ref.current.scrollTop;
-      const totalScrollHeight = ref.current.scrollHeight;
-      const pageHeight = ref.current.clientHeight;
+  const scrollToNextSlide = () => {
+    const top = ref.current.scrollTop;
+    const totalScrollHeight = ref.current.scrollHeight;
+    const pageHeight = ref.current.clientHeight;
 
-      // reached last page
-      if (top + pageHeight >= totalScrollHeight) {
-         // then scroll to top
-         ref.current.scrollTo({
-            behavior: "smooth",
-            top: 0,
-         });
-      } else {
-         // else go to next page
-         ref.current.scrollTo({
-            behavior: "smooth",
-            top: top + pageHeight,
-         });
-      }
-   };
-   const scrollToPreviousSlide = () => {
-      const top = ref.current.scrollTop;
-      const totalScrollHeight = ref.current.scrollHeight;
-      const pageHeight = ref.current.clientHeight;
+    // reached last page
+    if (top + pageHeight >= totalScrollHeight) {
+      // then scroll to top
+      ref.current.scrollTo({
+        behavior: "smooth",
+        top: 0,
+      });
+    } else {
+      // else go to next page
+      ref.current.scrollTo({
+        behavior: "smooth",
+        top: top + pageHeight,
+      });
+    }
+  };
+  const scrollToPreviousSlide = () => {
+    const top = ref.current.scrollTop;
+    const totalScrollHeight = ref.current.scrollHeight;
+    const pageHeight = ref.current.clientHeight;
 
-      // on first page
-      if (top <= 0) {
-         // then scroll to bottom
-         ref.current.scrollTo({
-            behavior: "smooth",
-            top: totalScrollHeight - pageHeight,
-         });
-      } else {
-         // else go to previous page
-         ref.current.scrollTo({
-            behavior: "smooth",
-            top: top - pageHeight,
-         });
-      }
-   };
-   // next slide mapping
-   useKey("Enter", scrollToNextSlide);
-   useKey("ArrowDown", scrollToNextSlide);
-   useKey("ArrowRight", scrollToNextSlide);
-   useKey("Space", scrollToNextSlide);
+    // on first page
+    if (top <= 0) {
+      // then scroll to bottom
+      ref.current.scrollTo({
+        behavior: "smooth",
+        top: totalScrollHeight - pageHeight,
+      });
+    } else {
+      // else go to previous page
+      ref.current.scrollTo({
+        behavior: "smooth",
+        top: top - pageHeight,
+      });
+    }
+  };
+  // next slide mapping
+  useKey("Enter", scrollToNextSlide);
+  useKey("ArrowDown", scrollToNextSlide);
+  useKey("ArrowRight", scrollToNextSlide);
+  useKey("Space", scrollToNextSlide);
 
-   // previous slide mapping
-   useKey("ArrowUp", scrollToPreviousSlide);
-   useKey("ArrowLeft", scrollToPreviousSlide);
-   useKey("Backspace", scrollToPreviousSlide);
-   useKey("Escape", scrollToPreviousSlide);
+  // previous slide mapping
+  useKey("ArrowUp", scrollToPreviousSlide);
+  useKey("ArrowLeft", scrollToPreviousSlide);
+  useKey("Backspace", scrollToPreviousSlide);
+  useKey("Escape", scrollToPreviousSlide);
 
-   return (
-      <context.Provider value={{ offsetY, notify }}>
-         <div
-            className={
-               "w-full h-screen relative snap-y snap-mandatory overflow-y-auto " +
-               className
-            }
-            ref={ref}
-            {...others}>
-            {children}
-         </div>
-      </context.Provider>
-   );
+  return (
+    <context.Provider value={{ offsetY, notify }}>
+      <div
+        className={
+          "w-full h-screen relative snap-y snap-mandatory overflow-y-auto " +
+          className
+        }
+        ref={ref}
+        {...others}
+      >
+        {children}
+      </div>
+    </context.Provider>
+  );
 };
 
 export const Slide = ({
-   className,
-   children,
-   slideRef,
-   msg,
-   icon,
-   ...others
+  className,
+  children,
+  slideRef,
+  msg,
+  icon,
+  ...others
 }) => {
-   const { notify, offsetY } = useContext(context);
+  const { notify, offsetY } = useContext(context);
 
-   useEffect(() => {
-      if (slideRef.current.offsetTop === offsetY && msg) {
-         notify(msg, icon);
-      }
-   }, [offsetY, icon, msg, notify, slideRef]);
+  useEffect(() => {
+    if (slideRef.current.offsetTop === Math.ceil(offsetY) && msg) {
+      notify(msg, icon);
+    }
+  }, [offsetY, icon, msg, notify, slideRef]);
 
-   return (
-      <div
-         className={"w-full min-h-full relative snap-start " + className}
-         ref={slideRef}
-         {...others}>
-         {children}
-      </div>
-   );
+  return (
+    <div
+      className={"w-full min-h-full relative snap-start " + className}
+      ref={slideRef}
+      {...others}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Slide;
